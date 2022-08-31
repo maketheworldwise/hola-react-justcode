d6e8127c402d471c96b5154e8fbe559bc22cfdde
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-31 14:01:34 +0900

    :sparkles: Post

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import "./Main.scss";
-import UserInfo from "../components/userInfo/UserInfo";
 import Banner from "../components/banner/Banner";
+import Post from "../components/post/Post";
 
 function Main() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    fetch("/mock/postList.json", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setPostList(res.data));
+  }, [postList]);
+
   return (
     <>
       <Banner />
-      <UserInfo userProfileImageUri={"https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG"} userName={"Test"} />
+      <div style={{ margin: "104px auto" }}>Category Filtering Area</div>
+      {postList.map((post) => {
+        return <Post key={post.id} data={post} />;
+      })}
     </>
   );
 }
